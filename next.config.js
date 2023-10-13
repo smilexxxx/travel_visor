a39087adbd7321cb351cc970b8a36b3f2a58b9da
smilexxxx <Brianmuiruri15@gmail.com>
2023-10-13 16:50:14 +0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+};
 
-module.exports = nextConfig
+(module.exports = {
+  images: {
+    unoptimized: true,
+  },
+}),
+  nextConfig;
+
+//module.exports = nextConfig;
